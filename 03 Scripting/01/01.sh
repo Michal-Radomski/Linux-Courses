beab808f7e8f69280ac6b423b641198ffc4ad098
Michal Radomski <michalra@op.pl>
2025-10-10 21:49:50 +0200

    Shell Scripting/Bash course - commit 02

---
+++ 03 Scripting/01/01.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Filename: 01.sh
+# Purpose: Testing
+# Author: MR
+# Date: 2025-10-10
+
+# run: bash 01.sh or ./01.sh
+clear
+echo test
+ls -l
+date
+echo $PATH
+
+# This is example shell script
+function TestFunction() {
+  echo "Inside Function Test Function"
+}
+
+pwd
+
+TestFunction
