077a1c4b0f4f3db783cea40b11a8ae6720a4a4d2
Michal Radomski <michalra@op.pl>
2025-10-10 22:09:12 +0200

    Shell Scripting/Bash course - commit 03

--- 03 Scripting/01/01.sh
+++ 03 Scripting/01/01.sh
@@ -20,3 +20,29 @@ function TestFunction() {
 pwd
 
 TestFunction
+
+#* Variables
+SIZE=256
+FILE="Security.config"
+#* Referencing
+echo $SIZE
+echo $FILE
+echo "The value of Size is $SIZE"
+echo "The value of Size is ${SIZE}MB"
+
+EMPTY_VAR=""
+EMPTY_VAR_2=''
+echo "EMPTY_VAR1 = NULL${EMPTY_VAR}NULL"
+echo "EMPTY_VAR2 = NULL${EMPTY_VAR_2}NULL"
+
+# Redirection of output ">"
+ls -l >test.txt
+echo "Test Message" >>test.txt
+
+wc -c <test.txt
+
+grep Message <test.txt
+
+ls -l | grep test
+
+cat test.txt | grep Test
