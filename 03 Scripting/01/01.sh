4ff6de1262b4b223a93327b774692f1c056e1002
Michal Radomski <michalra@op.pl>
2025-10-10 23:01:53 +0200

    Shell Scripting/Bash course - commit 04

--- 03 Scripting/01/01.sh
+++ 03 Scripting/01/01.sh
@@ -46,3 +46,59 @@ grep Message <test.txt
 ls -l | grep test
 
 cat test.txt | grep Test
+
+ls -l te*
+ls -l ????? # 5 characters -> 01.sh
+ls -l [t]*
+ls -l [a-zA-Z]*
+
+echo '----------------'
+
+variable="World"
+echo 'Hello $variable' # Output: Hello $variable
+
+variable="World"
+echo "Hello $variable" # Output: Hello World
+
+variable="Bash"
+
+echo 'Single quote: $variable'         # literal, no expansion
+echo "Double quote: $variable"         # expands variable
+echo "Escape \$ and \" with backslash" # shows $ and " literally
+echo \$var                             # $var
+
+echo '----------------'
+
+echo "pwd is $(pwd)"
+echo "Date is $(date)"
+
+pwd_output=$(pwd)
+date_output=$(date)
+
+echo "pwd is $pwd_output"
+echo "Date is $date_output"
+
+echo "------Integer Arithmetic--------"
+# 1. Using Arithmetic Expansion with $(( ... )):
+a=10
+b=3
+
+echo $((a + b)) # Addition: 13
+echo $((a - b)) # Subtraction: 7
+echo $((a * b)) # Multiplication: 30
+echo $((a / b)) # Division: 3
+echo $((a % b)) # Modulus: 1 (remainder)
+
+# 2. Using let command:
+let result=a+b
+echo $result # Output: 13
+
+let "result = a * b"
+echo $result # Output: 30
+
+# 3. Using expr command:
+sum=$(expr $a + $b)
+echo $sum # 13
+
+mul=$(expr $a \* $b) # Note the escaped *
+echo $mul            # 30
