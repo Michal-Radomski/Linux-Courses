695f39ba7b44e3888152056f5a7df8be0a1d0dc2
Michal Radomski <michalra@op.pl>
2025-10-12 00:19:02 +0200

    Shell Scripting/Bash course - commit 16

--- 03 Scripting/01/script03.sh
+++ 03 Scripting/01/script03.sh
@@ -46,3 +46,11 @@ until [[ $i -eq 5 ]]; do
   echo "$i"
   ((i++))
 done
+
+#  Nested Loop
+for ((i = 1; i <= 3; i++)); do
+  for ((j = 1; j <= 4; j++)); do
+    echo -n "$i "
+  done
+  echo
+done >>test.txt
