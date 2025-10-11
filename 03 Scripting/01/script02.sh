f009d166b4ecbe461eae5e13c83cc6958912c8f7
Michal Radomski <michalra@op.pl>
2025-10-11 21:46:42 +0200

    Shell Scripting/Bash course - commit 06

--- 03 Scripting/01/script02.sh
+++ 03 Scripting/01/script02.sh
@@ -5,6 +5,7 @@
 # Author: MR
 # Date: 2025-10-10
 
+clear
 ls -l # 0
 echo $?
 
@@ -16,3 +17,22 @@ echo $?  # ls: cannot access 'a*': No such file or directory
 
 xyz # 127
 echo $?
+
+echo "------------------------- if...then -------------------------"
+
+if grep Message test.txt; then
+  echo "Message Found"
+fi
+
+if ls | grep test; then
+  echo "test.txt"
+fi
+
+echo "----- test ---------"
+x=10
+y=10
+
+echo "x = $x, y = $y"
+if [ $x -ge $y ]; then
+  echo '[$x -ge $y] : True'
+fi
