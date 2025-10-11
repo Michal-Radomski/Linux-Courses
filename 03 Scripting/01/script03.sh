4438c7ddfb4869ae2d8126e5b3358900e4c04ab4
Michal Radomski <michalra@op.pl>
2025-10-11 23:58:56 +0200

    Shell Scripting/Bash course - commit 14

---
+++ 03 Scripting/01/script03.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Filename: script02.sh
+# Purpose: Testing
+# Author: MR
+# Date: 2025-10-11
+
+clear
+# For loop
+for i in {0..5}; do
+  echo "Element 1-> $i"
+done
+
+# Traditional C-style for loop
+for ((i = 0; i <= 5; i++)); do
+  echo "Element 2-> $i"
+done
+
+for ((x = 1, y = 5; x <= y; x++, y--)); do
+  echo "x=$x, y=$y"
+done
+
+# Reading a file
+for line in $(cat test.txt); do
+  echo $line
+done
