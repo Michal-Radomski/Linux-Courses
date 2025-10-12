238cf3f13c60728ed9fb249402c0344d2e69da48
Michal Radomski <michalra@op.pl>
2025-10-12 21:59:18 +0200

    Shell Scripting/Bash course - commit 23

--- 03 Scripting/01/script06.sh
+++ 03 Scripting/01/script06.sh
@@ -28,3 +28,22 @@ my_array+=("date")
 echo "New element added: ${my_array[3]}"
 
 echo "\${my_array[*]} =  ${my_array[*]}" # ${my_array[*]} =  apple blueberry cherry date
+
+# Array and Function
+# Define a function that takes an array as arguments
+print_elements() {
+  local my_array=("$@") # Capture all function arguments as an array
+  for i in "${!my_array[@]}"; do
+    echo "Element $i: ${my_array[i]}"
+  done
+}
+
+# Declare an array
+my_array=("apple" "banana" "cherry" "date")
+
+# Call the function and pass the array elements
+print_elements "${my_array[@]}"
+# Element 0: apple
+# Element 1: banana
+# Element 2: cherry
+# Element 3: date
