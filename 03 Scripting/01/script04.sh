c29349d0286a0e95ddab8c374c84c60bea3d81a8
Michal Radomski <michalra@op.pl>
2025-10-12 20:37:14 +0200

    Shell Scripting/Bash course - commit 19

--- 03 Scripting/01/script04.sh
+++ 03 Scripting/01/script04.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Filename: script03.sh
+# Filename: script04.sh
 # Purpose: Testing
 # Author: MR
 # Date: 2025-10-12
@@ -15,3 +15,16 @@ echo "All Arguments with \$*: $*"
 echo "All Arguments with \$@: $@"
 echo "Total Number of Arguments: $#"
 #* Using: bash <script_name.sh> arg1 arg2 arg3
+# Script Name: script04.sh
+# First Argument: arg1
+# Second Argument: arg2
+# All Arguments with $*: arg1 arg2 arg3
+# All Arguments with $@: arg1 arg2 arg3
+# Total Number of Arguments: 3
+
+# Shift command
+# Print all arguments one by one using shift
+while [ $# -gt 0 ]; do
+  echo "Current argument: $1"
+  shift
+done
