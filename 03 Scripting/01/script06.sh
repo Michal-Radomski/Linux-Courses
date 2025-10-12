c35a2fd0d6ad0955a2c9c0186bb3f4154fd87941
Michal Radomski <michalra@op.pl>
2025-10-12 21:50:47 +0200

    Shell Scripting/Bash course - commit 22

---
+++ 03 Scripting/01/script06.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Filename: script06.sh
+# Purpose: Testing - Arrays
+# Author: MR
+# Date: 2025-10-12
+
+clear
+# Arrays
+# Declare an array with elements
+my_array=("apple" "banana" "cherry")
+
+# Access array elements by index (starting at 0)
+echo "First element: ${my_array[0]}"
+echo "Second element: ${my_array[1]}"
+
+# Loop through all elements of the array
+for fruit in "${my_array[@]}"; do
+  echo "Fruit: $fruit"
+done
+
+# Modify an element
+my_array[1]="blueberry"
+echo "Modified second element: ${my_array[1]}"
+
+# Add a new element
+my_array+=("date")
+echo "New element added: ${my_array[3]}"
+
+echo "\${my_array[*]} =  ${my_array[*]}" # ${my_array[*]} =  apple blueberry cherry date
