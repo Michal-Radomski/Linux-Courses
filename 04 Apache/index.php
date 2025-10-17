7336a5aee1c5143225ddb8ee1123cfb05a6111d6
Michal Radomski <michalra@op.pl>
2025-10-17 22:29:32 +0200

    Apache Web Server course - commit 09

---
+++ 04 Apache/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Index Page</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Welcome to the Simple Index Page</h1>";
+        echo "<p>This is a basic PHP page that outputs HTML content.</p>";
+    ?>
+</body>
+</html>
