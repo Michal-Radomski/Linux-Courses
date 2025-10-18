a20d1cf20706b91a23483fcb6bdc2f2503ec3fad
Michal Radomski <michalra@op.pl>
2025-10-18 22:12:48 +0200

    Apache Web Server course - commit 11

--- 04 Apache/index.php
+++ 04 Apache/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,4 +10,20 @@
         echo "<p>This is a basic PHP page that outputs HTML content.</p>";
     ?>
 </body>
-</html>
+</html> -->
+
+<?php
+	$conn = mysqli_connect('localhost','aanya','Asdf#1234','employee');
+
+	$q = "select * from salary limit 4";
+
+	$result = mysqli_query($conn,$q);
+
+	while ($row = mysqli_fetch_array($result))
+		{
+			echo $row['eno']." ";
+			echo $row['dept']." ";
+			echo $row['salary']." ";
+			echo "<br />";
+		}
+?>
