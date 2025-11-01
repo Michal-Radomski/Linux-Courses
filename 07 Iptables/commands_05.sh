4ebfe5070f0d3f35af3fa1b5c6a9bc2aebf64ed3
Michal Radomski <michalra@op.pl>
2025-11-01 20:53:41 +0100

    Iptables course - commit 13

--- 07 Iptables/commands_05.sh
+++ 07 Iptables/commands_05.sh
@@ -57,3 +57,77 @@ iptables -X
 echo "Flushing and destroying all ip sets."
 ipset -F
 ipset -X
+
+#* Script - Block all IPs and Networks From File
+#!/bin/bash
+
+echo "### BLOCKING ALL IPs AND NETWORKS FROM FILE."
+
+# a file called bad_hosts.txt exists in the same directory with the script and
+# contains IPs and Networks, one per line like:
+# 11.0.0.16
+# 8.8.8.8
+# 1.2.3.4
+# 192.0.0.0/16
+
+# File that contains the IPs and Nets to block
+FILE="bad_hosts.txt"
+
+# Creating a new set
+ipset -N bad_hosts iphash -exist
+
+# Flushing the set if it exists
+ipset -F bad_hosts
+
+echo "Adding IPs from $FILE to bad_hosts set:"
+for ip in $(cat $FILE); do
+  ipset -A bad_hosts $ip
+  echo -n "$ip "
+done
+
+# Flush iptables if you run this script more than once to avoid duplicate rules
+# iptables -F
+
+# Adding the iptables rule that references the set and drops all ips and nets
+echo -e -n "\nDropping with iptables... "
+iptables -I INPUT -m set --match-set bad_hosts src -j DROP
+echo "Done"
+
+#* Script - Blocking Countries (eg China)
+#!/bin/bash
+
+echo "### BLOCKING CHINA ###"
+
+# Check if the file exists (in the current directory) and if yes, remove it
+if [ -f "cn-aggregated.zone" ]; then
+  rm cn-aggregated.zone
+fi
+
+# Download the aggregate zone file for China
+wget http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone
+
+# Check if there was an error
+if [ $? -eq 0 ]; then
+  echo "Download Finished!"
+else
+  echo "Download Failed! Exiting ..."
+  exit 1
+
+fi
+
+# Creating a new set called china of type hash:net (nethash)
+ipset -N china hash:net -exist
+
+# Flushing the set
+ipset -F china
+
+# Iterate over the Networks from the file and add them to the set
+echo "Adding Networks to set..."
+for i in $(cat cn-aggregated.zone); do
+  ipset -A china $i
+done
+
+# Adding a rule that references the set and drops based on source IP address
+echo -n "Blocking CN with iptables ... "
+iptables -I INPUT -m set --match-set china src -j DROP
+echo "Done"
