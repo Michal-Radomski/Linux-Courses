6924886dde86c14050bd1754a80a45bfb33e7b73
Michal Radomski <michalra@op.pl>
2025-11-02 22:40:18 +0100

    Iptables course - commit 21

--- 07 Iptables/commands_09.sh
+++ 07 Iptables/commands_09.sh
@@ -123,3 +123,23 @@ iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 #set default policy DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
+
+#---
+#!/bin/bash
+
+echo -n 'Enter the IP or Network Address: '
+read IP
+
+echo -n 'Enter DROP or ACCEPT: '
+read ANSWER
+
+if [ $ANSWER = "DROP" ]; then
+  iptables -I INPUT -s $IP -j DROP
+  echo "Traffic from $IP has been dropped"
+elif [ $ANSWER = "ACCEPT" ]; then
+
+  iptables -I INPUT -s $IP -j ACCEPT
+  echo "Traffic from $IP has been accepted"
+else
+  echo "Invalid option. Enter DROP or ACCEPT"
+fi
