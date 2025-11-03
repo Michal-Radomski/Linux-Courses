9ffb76f7b8d832fb9135f48c7ad5fdb8f02ca6ba
Michal Radomski <michalra@op.pl>
2025-11-03 20:34:50 +0100

    Iptables course - commit 24

--- 07 Iptables/Appendix/script_01.sh
+++ 07 Iptables/Appendix/script_01.sh
@@ -33,3 +33,55 @@ set | grep Linux
 # unset distro
 
 # declare -r logdir="/var/low" #* -> read only variable
+
+read -p "Enter your name: " name
+echo $name
+
+read -s -p "Enter password: " password
+echo $password
+
+num=10
+if [ $num -gt 10 ]; then
+  echo "Number is greater than 10"
+elif [ $num -eq 10 ]; then
+  echo "Number is exactly 10"
+else
+  echo "Number is less than 10"
+fi
+
+str="hello"
+if [ "$str" == "hello" ]; then
+  echo "String is hello"
+elif [ "$str" == "world" ]; then
+  echo "String is world"
+else
+  echo "String is something else"
+fi
+
+read -p "Enter your age: " age
+read -p "Enter your membership status (yes/no): " member
+
+# Check if age is a positive number and less than or equal to 120
+if [[ $age -gt 0 && $age -le 120 ]]; then
+  echo "Valid age entered."
+
+  # Nested if to check membership status
+  if [[ $member == "yes" ]]; then
+    # Multiple conditions using elif inside nested if
+    if [[ $age -lt 18 ]]; then
+      echo "You are a minor member."
+    elif [[ $age -le 65 ]]; then
+      echo "You are an adult member."
+    else
+      echo "You are a senior member."
+    fi
+
+  elif [[ $member == "no" ]]; then
+    echo "You are not a member."
+  else
+    echo "Invalid membership status entered."
+  fi
+
+else
+  echo "Invalid age entered."
+fi
